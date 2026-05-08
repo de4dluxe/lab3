a5ba24981b1ca17abda4b05e9cdd122a82288260
de4dluxe <sas54ru@gmail.com>
2026-05-08 12:19:16 +0700

    Modified task5.cpp

--- task5/task5.cpp
+++ task5/task5.cpp
@@ -22,7 +22,6 @@ void linear_regression(const vector<double>& x,
     double sum_y = 0;
     double sum_xy = 0;
     double sum_x2 = 0;
-    double sum_y2 = 0;
 
     // Подсчёт сумм
     for (int i = 0; i < n; i++) {
@@ -30,19 +29,37 @@ void linear_regression(const vector<double>& x,
         sum_y += y[i];
         sum_xy += x[i] * y[i];
         sum_x2 += x[i] * x[i];
-        sum_y2 += y[i] * y[i];
     }
+
+    // Средние значения
+    double mean_x = sum_x / n;
+    double mean_y = sum_y / n;
+
+    // Переменные для корреляции
+    double numerator = 0;
+    double denominator_x = 0;
+    double denominator_y = 0;
+
+    // Формула корреляции Пирсона
+    for (int i = 0; i < n; i++) {
+
+        numerator += (x[i] - mean_x) *
+                     (y[i] - mean_y);
+
+        denominator_x += pow(x[i] - mean_x, 2);
+
+        denominator_y += pow(y[i] - mean_y, 2);
+    }
+
+    // Коэффициент корреляции
+    corr = numerator /
+           sqrt(denominator_x * denominator_y);
 
     // Коэффициенты прямой y = a*x + b
     a = (n * sum_xy - sum_x * sum_y) /
         (n * sum_x2 - sum_x * sum_x);
 
     b = (sum_y - a * sum_x) / n;
-
-    // Коэффициент корреляции
-    corr = (n * sum_xy - sum_x * sum_y) /
-           sqrt((n * sum_x2 - sum_x * sum_x) *
-                (n * sum_y2 - sum_y * sum_y));
 
     // Коэффициент детерминации
     R2 = corr * corr;
@@ -90,7 +107,7 @@ int main() {
     cout << fixed << setprecision(4);
 
     cout << "\nТаблица значений:\n";
-    cout << "t\tT\n";
+    cout << "t\t\tT\n";
 
     for (int i = 0; i < n; i++) {
         cout << times[i] << "\t" << temps[i] << endl;
